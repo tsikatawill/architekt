dfd1ce402f39cca51f688f899103611ce87803fb
William M. Tsikata <tsikatawill@gmail.com>
2022-12-29 17:56:00 +0000

    chore: remove unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import { About } from "../components/About";
 import { Contact } from "../components/Contact";
 import Head from "next/head";
 import { Hero } from "../components/Hero";
 import { Objectives } from "../components/Objectives";
 import { Projects } from "../components/Projects";
-import { styled } from "../stitches.config";
 
 export default function Home() {
   return (
