e5b259f6b9553ddfea5683ee509753aa8f3a8c0f
William M. Tsikata <tsikatawill@gmail.com>
2022-12-29 17:52:24 +0000

    feat: add gallery items

--- components/Hero/data.js
+++ components/Hero/data.js
@@ -20,3 +20,106 @@ export const carouselItems = [
     link: "/projects/project-nurtown",
   },
 ];
+
+export const galleryItems = [
+  {
+    image: "/images/our-projects/project-1.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-2.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-3.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-4.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-5.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-6.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-7.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-8.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-1.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-2.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-3.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-4.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-5.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-6.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-7.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-8.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-1.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-2.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-3.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+  {
+    image: "/images/our-projects/project-4.jpg",
+    title:
+      "Skinsky was chosen at all to aid in the work? In my husband's diary, Skinsky is mentioned as dealing with the Slovaks who trade down the river.",
+  },
+];
