056b5846aa4c80f68ba8e7a43df5ef930e32ae69
William M. Tsikata <tsikatawill@gmail.com>
2022-12-31 03:54:03 +0000

    style: clamp text

--- components/SectionHeader.jsx
+++ components/SectionHeader.jsx
@@ -2,8 +2,12 @@ import { motion } from "framer-motion";
 import { styled } from "../stitches.config";
 
 export const SectionHeader = styled(motion.h2, {
-  fontSize: "$6",
   fontWeight: "$light",
   color: "$primaryLight",
   textTransform: "capitalize",
+  fontSize: "40px",
+
+  "@sm": {
+    fontSize: "clamp(40px, 6vw, $6)",
+  },
 });
